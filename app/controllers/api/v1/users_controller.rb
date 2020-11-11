c9533ed08c1be965b040eafa1ff637a6c9e2c66d
mayalexandera <maya.blevins@gmail.com>
2020-11-11 15:08:52 -0500

    commit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,37 +1,30 @@
-
 class Api::V1::UsersController < ApplicationController
-before_action :set_user, except: [:new, :create, :index]
+
   def index
     users = User.all
   end
 
   def create
-    @user = User.new(user_params)
-    @user.password=(params[:password])
+    user = User.new(user_params)
+    user.password=(params[:password])
 
-    if @user.save
-      Cart.create!(member_id: @user.id)
-      login!(@user)
-      render json: @user
+    if user.save
+      login!(user)
+      Cart.create!(member_id: user.id)
+      render json: user
     else
-      render json: @user.errors.full_messages, status: 401
+      render json: user.errors.full_messages, status: 401
     end
 
   end
 
   def show
-    cart = @user.cart
-    cart_items = cart.cart_items
-    render json: { user: @user, cart: cart, cart_items: cart_items}
+    user = User.find_by(id: params[:id])
+    render json: { user: user }
   end
   
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password)
   end
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
 end
